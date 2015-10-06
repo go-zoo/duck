5a41eed83999078fb9dee2bb62311be48a6211a1
Jonathan <jonathan.chaput@voxlog.ca>
2015-10-06 16:34:08 -0400

    update

--- duck.go
+++ duck.go
@@ -15,8 +15,8 @@ var (
 )
 
 func SetContext(req *http.Request, key, value interface{}) {
+	mutex.Lock()
 	if data[req] == nil {
-		mutex.Lock()
 		data[req] = &context{values: make(map[interface{}]interface{})}
 		mutex.Unlock()
 	}
@@ -27,20 +27,20 @@ func SetContext(req *http.Request, key, value interface{}) {
 }
 
 func GetContext(req *http.Request, key interface{}) interface{} {
+	mutex.RLock()
 	if data[req] == nil {
 		return nil
 	}
-	mutex.RLock()
 	value := data[req].values[key]
 	mutex.RUnlock()
 	return value
 }
 
 func GetAllContext(req *http.Request) map[interface{}]interface{} {
+	mutex.RLock()
 	if data[req] == nil {
 		return nil
 	}
-	mutex.RLock()
 	values := data[req].values
 	mutex.RUnlock()
 	return values
@@ -55,7 +55,9 @@ func DeleteContext(req *http.Request, key interface{}) {
 }
 
 func Clear(req *http.Request) {
+	mutex.Lock()
 	delete(data, req)
+	mutex.Unlock()
 }
 
 func UseContext(next http.Handler) http.Handler {
